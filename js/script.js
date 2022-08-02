964f58f9c51e02d211a682b31aa1bb1321e41520
Jibé Dev <jibaoke@gmail.com>
2022-08-02 16:18:29 +0200

    scroll to top add

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+  var scroll = document.querySelector('.scrollTop');
+  scroll.classList.toggle('active', window.scrollY > 1000);
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+  });
+}
